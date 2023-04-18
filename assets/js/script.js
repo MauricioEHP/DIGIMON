c20c9486071da1e0e180035a3e8f049c737914c1
mauricio Henriquez Pino <mauriciohenriquezp@gmail.com>
2023-04-18 02:47:58 -0400

    commit digimon4

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+var url = "https://digimon-api.vercel.app/api/digimon" 
+var contenido = document.querySelector(".contenido")
+
+fetch(url)
+.then(response => response.json())
+.then(datos => {
+    for (item of datos) {
+        
+            contenido.innerHTML +=  `
+            <div class="tarjeta">
+            <div class="card" style="width: 18rem; ">
+            <img src="${item.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${item.name}</h5>
+              <p class="card-text">${item.level}</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+            </div>
+            `
+            if (item.id == 3) {break;}
+        }
+    
+})
