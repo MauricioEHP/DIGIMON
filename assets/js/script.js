d284f098060282ff7304043ba1cddae132f92fdd
mauricio Henriquez Pino <mauriciohenriquezp@gmail.com>
2023-04-23 21:45:03 -0400

    commit9

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,71 @@
-var url = "https://digimon-api.vercel.app/api/digimon" 
-var contenido = document.querySelector(".contenido")
+var api_url = "https://digimon-api.vercel.app/api/digimon"
+var contenido = document.querySelector("#contenido")
 
-fetch(url)
+var leveles = document.querySelector("#leveles")
+var main =document.getElementsByTagName("main")
+var scajaPrincipal = document.getElementById("cajaPrincipal")
+
+function insertaF(){
+scajaPrincipal.style.backgroundImage = "url('C:\Digimon\assets\img\digiworld.png')";
+
+}
+
+
+
+fetch(api_url)
 .then(response => response.json())
 .then(datos => {
     for (item of datos) {
         
             contenido.innerHTML +=  `
             <div class="tarjeta">
-            <div class="card" style="width: 10rem; ">
+            <div class="card" style="width: 18rem; ">
             <img src="${item.img}" class="card-img-top" alt="...">
             <div class="card-body">
-              <h7 class="card-title">${item.name}</h7>
+              <h5 class="card-title">${item.name}</h5>
               <p class="card-text">${item.level}</p>
-             
+              <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
             </div>
             </div>
             `
             if (item.id == 3) {break;}
         }
-       // <a href="#" class="btn btn-primary">Go somewhere</a> 
+    
 })
 
+function niveles(){
+    let levels = document.getElementById('levels');
+    let nivel =levels.value;
+    var porniveles = document.querySelector("#porniveles")
+    
+      console.log(`${api_url}/level/${nivel}`)  
+    }
+    var btnFetch =document.getElementById('btnFetch');
+    btnFetch.addEventListener("click",function(){
+        fetch(`${api_url}/level/${nivel}`)
+          .then(response => response.json())
+          .then(datos => {
+              for (item of datos) {
+          
+              leveles.innerHTML +=  `
+              <div class="tarjeta">
+              <div class="card" style="width: 18rem; ">
+              <img src="${item.img}" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">${item.name}</h5>
+                <p class="card-text">${item.level}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+              </div>
+              </div>
+              `
+              if (item.id == 3) {break;}
+          }
+      })
+})
+
+
+
+
+ 
